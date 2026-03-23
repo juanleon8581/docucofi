436f936354f97f3369b2e4a6af2f7847ee1a85a8
Juan Pablo Leon <juanleon8581@gmail.com>
2026-03-23 10:39:31 -0500

    ♻️ refactor: add className prop support to CollapsiblePanel

--- src/presentation/components/CollapsiblePanel/CollapsiblePanel.tsx
+++ src/presentation/components/CollapsiblePanel/CollapsiblePanel.tsx
@@ -7,16 +7,21 @@ import {
 
 import "@/presentation/styles/collapsiblePanel.css";
 import { ScrollArea } from "../ui/scroll-area";
+import { cn } from "@/presentation/lib/utils";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const CollapsiblePanel = ({ children }: Props) => {
+export const CollapsiblePanel = ({ children, className }: Props) => {
   return (
     <Collapsible
       data-testid="cp-container"
-      className="cp-container mx-auto flex min-h-10 w-11/12 flex-col gap-2 rounded-b-3xl bg-primary p-2 text-primary-foreground shadow-lg shadow-primary/50 md:mx-0 md:h-full md:w-auto md:min-w-10 md:flex-row-reverse md:rounded-l-3xl! md:rounded-b-none"
+      className={cn(
+        "cp-container mx-auto flex min-h-10 w-11/12 flex-col gap-2 rounded-b-3xl bg-primary p-2 text-primary-foreground shadow-lg shadow-primary/50 md:mx-0 md:h-full md:w-auto md:min-w-10 md:flex-row-reverse md:rounded-l-3xl! md:rounded-b-none",
+        className,
+      )}
     >
       <CollapsibleTrigger
         data-testid="cp-trigger"
