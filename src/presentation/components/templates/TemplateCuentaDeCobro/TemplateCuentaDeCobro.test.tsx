7ab0f06810880c7d9d523417d4dfc16c75b2364c
Juan Pablo Leon <juanleon8581@gmail.com>
2026-03-23 10:39:37 -0500

    ✨ feat: add TemplateCuentaDeCobro template component

---
+++ src/presentation/components/templates/TemplateCuentaDeCobro/TemplateCuentaDeCobro.test.tsx
@@ -0,0 +1,45 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi, beforeEach } from "vitest";
+import { TemplateCuentaDeCobro } from "./TemplateCuentaDeCobro";
+
+describe("TemplateCuentaDeCobro.test", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    cleanup();
+    render(<TemplateCuentaDeCobro />);
+  });
+
+  describe("container", () => {
+    it("should render the template container", () => {
+      const container = screen.getByTestId("template-cdc-container");
+      expect(container).toBeInTheDocument();
+    });
+
+    it("should have the correct styles", () => {
+      const expectedClassList = [
+        "template-container",
+        "h-full",
+        "items-center",
+        "justify-between",
+        "pb-4",
+        "md:p-4",
+        "md:pr-0",
+      ];
+      const container = screen.getByTestId("template-cdc-container");
+      const classList = container.classList;
+
+      expect(classList.length).toBe(expectedClassList.length);
+      expectedClassList.forEach((t) => {
+        expect(classList).toContain(t);
+      });
+    });
+
+    it("should have the correct children", () => {
+      const container = screen.getByTestId("template-cdc-container");
+      const children = container.children as HTMLCollectionOf<HTMLElement>;
+      expect(children.length).toBe(2);
+      expect(children[0].dataset.testid).toBe("cp-container");
+      expect(children[1].dataset.testid).toBe("previewer-container");
+    });
+  });
+});
---
+++ src/presentation/components/templates/TemplateCuentaDeCobro/TemplateCuentaDeCobro.tsx
@@ -0,0 +1,62 @@
+import { CollapsiblePanel } from "@/presentation/components/CollapsiblePanel/CollapsiblePanel";
+import { Previewer } from "../../Previewer/Previewer";
+
+export const TemplateCuentaDeCobro = () => {
+  return (
+    <div
+      data-testid="template-cdc-container"
+      className="template-container h-full justify-between pb-4 md:p-4 md:pr-0"
+    >
+      <CollapsiblePanel className="">
+        <p>Futuro formulario, contenido temporal.</p>
+      </CollapsiblePanel>
+
+      <div className="flex w-full justify-center">
+        <Previewer>
+          <div className="m-6">
+            <strong className="right-block">
+              INSERTE CIUDAD, 20 de marzo de 2026
+            </strong>
+            <span className="center-title mt-16">INSERTE EMPRESA S.A.S</span>
+            <span className="center-subtitle mb-16">NIT: INSERTE NIT</span>
+            <strong className="left-block">DEBE A:</strong>
+            <span className="left-block accent-text">INSERTE NOMBRE</span>
+            <span className="left-block mb-8">
+              <span className="accent-text">CC</span> INSERTE CC
+            </span>
+            <span className="left-block">
+              <span className="accent-text">LA SUMA DE:</span> $ 0
+            </span>
+            <span className="left-block mb-8 text-sm">
+              INSERTE VALOR EN LETRAS
+            </span>
+            <strong className="left-block">CONCEPTO:</strong>
+            <span className="left-block mb-8">INSERTE CONCEPTO</span>
+            <span className="left-block">Por favor consignar en:</span>
+            <span className="left-block">
+              Cuenta de <span className="accent-text">ahorros</span> No.{" "}
+              <span className="accent-text">1234567890</span>
+            </span>
+            <span className="left-block accent-text mb-8">INSERTE BANCO</span>
+            <span className="left-block mb-16">Atentamente,</span>
+            <strong className="left-block mb-8">
+              ____________________________________
+            </strong>
+            <strong className="left-block mb-2">Información tributaria</strong>
+            <span className="left-block mb-1">
+              No soy responsable de IVA, ni de IMPOCONSUMO y no estoy obligado a
+              facturar.
+            </span>
+            <p>
+              De acuerdo con lo establecido en el parágrafo 2 del artículo 383
+              del estatuto tributario, declaro que para la prestación del
+              servicio no he contratado o vinculado dos (2) personas o más
+              trabajadores asociados a dicha actividad. Solicito que lo anterior
+              sea tenido en cuenta para efectos de retención en la fuente.
+            </p>
+          </div>
+        </Previewer>
+      </div>
+    </div>
+  );
+};
